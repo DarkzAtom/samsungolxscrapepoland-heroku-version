e8d51a60942249f9648f16b0259c7a9a1d658a0f
DarkzAtom <rekrut.fedosenko@gmail.com>
2023-12-29 03:18:06 +0100

    updated arguments for Chromeoptions to adjust it to Heroku requirements

--- scrapeinlist.py
+++ scrapeinlist.py
@@ -9,6 +9,9 @@ def insidelist(url):
 
     options = ChromeOptions()
     options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     time.sleep(5)
